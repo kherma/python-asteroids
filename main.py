bac682de19bdf2bd10d4e23977d749332fd6bad2
kherma <kamil.herma01@gmail.com>
2026-03-12 17:31:04 +0100

    Implement Player class and integrate into main game loop

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 from logger import log_state
+from player import Player
 
 
 def main():
@@ -12,6 +13,9 @@ def main():
     clock = pygame.time.Clock()
     dt=0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    x= SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    player = Player(x, y)
 
     while True:
         log_state()
@@ -19,10 +23,10 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
         delta_time = clock.tick(60)
         dt = delta_time / 1000
-        print(dt)
         
 
 
