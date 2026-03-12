b958c71d0481fab4f1a629d0755711f4593d1f60
kherma <kamil.herma01@gmail.com>
2026-03-12 20:49:00 +0100

    Implement asteroid splitting logic and update collision handling in main game loop

--- asteroid.py
+++ asteroid.py
@@ -1,6 +1,8 @@
 from circleshape import CircleShape
-from constants import LINE_WIDTH
+from constants import LINE_WIDTH, ASTEROID_MIN_RADIUS
 import pygame
+from logger import log_event
+import random
 
 class Asteroid(CircleShape):
     def __init__(self, x, y, radius):
@@ -11,3 +13,17 @@ class Asteroid(CircleShape):
 
     def update(self, dt):
         self.position += self.velocity * dt
+
+    def split(self):
+        self.kill()
+        if(self.radius <= ASTEROID_MIN_RADIUS):
+            return
+        log_event("asteroid_split")
+        random_angle = random.uniform(20,50)
+        new_vector_1 = pygame.math.Vector2.rotate(self.velocity, random_angle) * 1.2
+        new_vector_2 = pygame.math.Vector2.rotate(self.velocity, -random_angle) * 1.2
+        new_radius = self.radius - ASTEROID_MIN_RADIUS
+        new_asteroid_1 = Asteroid(self.position.x, self.position.y, new_radius)
+        new_asteroid_1.velocity = new_vector_1
+        new_asteroid_2 = Asteroid(self.position.x, self.position.y, new_radius)
+        new_asteroid_2.velocity = new_vector_2
--- main.py
+++ main.py
@@ -45,7 +45,7 @@ def main():
                 if(shot.collides_with(asteroid)):
                     log_event("asteroid_shot")
                     shot.kill()
-                    asteroid.kill()
+                    asteroid.split()
         screen.fill("black")
         for sprite in drawable:
             sprite.draw(screen)
