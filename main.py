732b27602db2b3ac0c2a3e33d650eaba221091fb
kherma <kamil.herma01@gmail.com>
2026-03-12 19:40:21 +0100

    Add Asteroid and AsteroidField classes with spawning logic and integrate into main game loop

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import pygame
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 from logger import log_state
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 
 def main():
@@ -17,8 +19,12 @@ def main():
     y = SCREEN_HEIGHT / 2
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
     Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
     player = Player(x, y)
+    asteroidField = AsteroidField()
 
     while True:
         log_state()
